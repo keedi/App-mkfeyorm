5941577e1fa87a498f27a6c761e5b9c979201027
Keedi Kim <keedi.k@gmail.com>
2011-01-15 16:52:34 +0900

    Generate Fey::ORM based module on the fly

--- bin/mkfeyorm.pl
+++ bin/mkfeyorm.pl
@@ -2,27 +2,88 @@
 # ABSTRACT: Make skeleton code with Fey::ORM
 # PODNAME: mkfeyorm.pl
 
-use 5.010;
 use utf8;
 use strict;
 use warnings;
 use autodie;
 use Getopt::Long::Descriptive;
-
-binmode STDIN,  ':utf8';
-binmode STDOUT, ':utf8';
+use App::mkfeyorm;
 
 my ( $opt, $usage ) = describe_options(
     "%c %o ...",
-    [ 'xxx', '' ],
+    [ 'schema|s=s',         'schema module name'                        ],
+    [ 'tables|t=s@',        'table module name list', { default => [] } ],
+    [ 'namespace|n=s',      'base namespace'                            ],
+    [ 'table_namespace=s',  'base table namespace'                      ],
+    [ 'schema_namespace=s', 'base schema namespace'                     ],
+    [
+        'output_path|o=s',
+        'base directory (default: lib/)',
+        { default => 'lib' },
+    ],
     [],
     [ 'verbose|v', 'print extra stuff', { default => 0 } ],
     [ 'help|h',    'print usage message and exit'        ],
 );
 
-print($usage->text), exit if $opt->help;
+print($usage->text), exit                          if     $opt->help;
+print("must specify schema\n", $usage->text), exit unless $opt->schema;
+print("must specify tables\n", $usage->text), exit unless $opt->tables;
 
-while (<>) {
-}
+my $app = App::mkfeyorm->new(
+    schema           => $opt->schema,
+    tables           => $opt->tables,
+    output_path      => $opt->output_path,
+    namespace        => $opt->namespace        || q{},
+    table_namespace  => $opt->table_namespace  || q{},
+    schema_namespace => $opt->schema_namespace || q{},
+);
+
+$app->process;
 
 __END__
+
+=head1 SYNOPSIS
+
+$ mkfeyorm.pl \
+    --namespace MedicalCoding::Test \
+    --table_namespace Model \
+    --schema Schema \
+    --table AE::Source \
+    --table AE::Task \
+    --table CM::Source \
+    --table CM::Task \
+    --table MC::User \
+    --table MC::Role \
+    --table MC::UserRole
+
+
+=head1 DESCRIPTION
+
+This is a L<App::mkfeyorm> wrapper script.
+At least C<--schema> and C<--table> options are needed.
+
+
+=head1 OPTIONS
+
+    mkfeyorm.pl [-hnostv] [long options...] ...
+        -s --schema             schema module name
+        -t --tables             table module name list
+        -n --namespace          base namespace
+        --table_namespace       base table namespace
+        --schema_namespace      base schema namespace
+        -o --output_path        base directory (default: lib/)
+    
+        -v --verbose            print extra stuff
+        -h --help               print usage message and exit
+
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Fey::ORM>
+
+=item L<App::mkfeyorm>
+
+=back
--- lib/App/mkfeyorm.pm
+++ lib/App/mkfeyorm.pm
@@ -1,6 +1,236 @@
-use strict;
-use warnings;
 package App::mkfeyorm;
 # ABSTRACT: Make skeleton code with Fey::ORM
 
+use Moose;
+use MooseX::SemiAffordanceAccessor;
+use MooseX::StrictConstructor;
+use namespace::autoclean;
+use autodie;
+
+use Template;
+use File::Basename;
+use File::Spec::Functions;
+
+( my $TEMPLATE_DIR = $INC{'App/mkfeyorm.pm'} ) =~ s/\.pm$//;
+my $SCHEMA_TEMPLATE = 'schema.tt';
+my $TABLE_TEMPLATE  = 'table.tt';
+
+has 'schema' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has 'tables' => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+);
+
+has 'output_path' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'lib',
+);
+
+has 'namespace' => (
+    is      => 'ro',
+    isa     => 'Str',
+);
+
+has 'table_namespace' => (
+    is      => 'ro',
+    isa     => 'Str',
+);
+
+has 'schema_namespace' => (
+    is      => 'ro',
+    isa     => 'Str',
+);
+
+has 'template_path' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => $TEMPLATE_DIR,
+);
+
+has 'schema_template' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => $SCHEMA_TEMPLATE,
+);
+
+has 'table_template' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => $TABLE_TEMPLATE,
+);
+
+has '_template' => (
+    is         => 'ro',
+    isa        => 'Template',
+    lazy_build => 1,
+);
+
+sub _build__template {
+    my $self = shift;
+
+    my $tt = Template->new({
+        INCLUDE_PATH     => $self->template_path,
+        OUTPUT_PATH      => $self->output_path,
+        DEFAULT_ENCODING => 'utf-8',
+    }) || die "$Template::ERROR\n";
+
+    return $tt;
+}
+
+sub process {
+    my $self = shift;
+
+    $self->_process_schema;
+    $self->_process_table($_) for @{ $self->tables };
+}
+
+sub _process_schema {
+    my $self = shift;
+
+    my $schema = join(
+        '::',
+        grep { $_ } (
+            $self->namespace,
+            $self->schema_namespace,
+            $self->schema,
+        )
+    );
+
+    my @tables = map {
+        join(
+            '::',
+            grep { $_ } ( $self->namespace, $self->table_namespace, $_ )
+        );
+    } @{$self->tables};
+
+    my $vars = {
+        SCHEMA    => $schema,
+        TABLES    => \@tables,
+    };
+
+    $self->_template->process(
+        $self->schema_template,
+        $vars,
+        $self->_gen_module_path($schema),
+    ) or die $self->_template->error, "\n";
+}
+
+sub _process_table {
+    my ( $self, $orig_table ) = @_;
+
+    my $schema = join(
+        '::',
+        grep { $_ } (
+            $self->namespace,
+            $self->schema_namespace,
+            $self->schema
+        )
+    );
+
+    my $table = join(
+        '::',
+        grep { $_ } (
+            $self->namespace,
+            $self->table_namespace,
+            $orig_table,
+        )
+    );
+
+    my $db_table = $orig_table;
+    $db_table =~ s/([A-Z]+)::/"_\L$1_"/ge;
+    $db_table =~ s/([A-Z]+)([A-Z])/"_\L$1_$2"/ge;
+    $db_table =~ s/([A-Z])/"_\L$1"/ge;
+    $db_table =~ s/::/_/g;
+    $db_table =~ s/_+/_/g;
+    $db_table =~ s/^_//;
+
+    my $vars = {
+        SCHEMA   => $schema,
+        TABLE    => $table,
+        DB_TABLE => $db_table,
+    };
+
+    $self->_template->process(
+        $self->table_template,
+        $vars,
+        $self->_gen_module_path($table),
+    ) or die $self->_template->error, "\n";
+}
+
+sub _gen_module_path {
+    my ( $self, $module ) = @_;
+
+    return catfile( split(/::/, $module) ) . '.pm';
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 1;
+__END__
+
+=head1 SYNOPSIS
+
+
+=attr schema
+
+Schema module name
+
+
+=attr tables
+
+Table module name list
+
+
+=attr output_path
+
+Output path for generated modules
+
+
+=attr namespace
+
+Namespace for schema and table module
+
+
+=attr table_namespace
+
+Namespace for table module
+
+
+=attr schema_namespace
+
+Namespace for schema module
+
+
+=attr template_path
+
+Template path. Default is the module installed directory.
+If you want to use your own template file then use this attribute.
+
+
+=attr schema_template
+
+Schema template file. Default is 'schema.tt'
+If you want to use your own template file then use this attribute.
+
+
+=attr table_template
+
+Table template file. Default is 'table.tt'
+If you want to use your own template file then use this attribute.
+
+
+=method process
+
+Make the skeleton perl module.
+
+
+=head1 SEE ALSO
+
+L<Fey::ORM>
