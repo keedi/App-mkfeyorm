c4ffad3ea03efc712374dee777b33aaae74162cd
Keedi Kim <keedi.k@gmail.com>
2011-08-30 14:37:26 +0900

    Work-around statistics_info() method call
    Since DBD::mysql 4.019, statistics_info() method is defined,
    so Fey::Loader cannot fix up statistics_info(), but still we
    cannot call statistics_info() method.  Now fix up code will be
    embed into auto generated schema module, so we can work around
    the problem.

--- lib/App/mkfeyorm.pm
+++ lib/App/mkfeyorm.pm
@@ -456,7 +456,18 @@ sub _load_schema {
         $schema = retrieve($params{cache_file});
     }
     else {
-        $schema = Fey::Loader->new( dbh => $source->dbh )->make_schema;
+        my $loader = Fey::Loader->new( dbh => $source->dbh );
+        my $dbh = $loader->dbh;
+        if (   $dbh
+            && $dbh->{Driver}
+            && $dbh->{Driver}{Name}
+            && $loader->dbh->{Driver}{Name} eq 'mysql' )
+        {
+            eval 'use DBD::mysql';
+            no warnings 'redefine';
+            *DBD::mysql::db::statistics_info = \&DBD::mysql::Fixup::_statistics_info;
+        }
+        $schema = $loader->make_schema;
     }
 [% ELSE -%]
     my $schema = Fey::Loader->new( dbh => $source->dbh )->make_schema;
